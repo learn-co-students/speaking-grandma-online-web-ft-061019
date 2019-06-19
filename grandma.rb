6a16e9f84396cbdd451f38dd8099bdbc4604770b
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-19 19:55:12 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,17 +1,10 @@
-require_relative = /grandma.rb
-
-if speak_to_grandma == 'i love you grandma!'.upcase
-  puts 'I love you to pumpkin'.upcase
-elsif  speak_to_grandma == 'Hi Nana,how are you?'
-  puts 'huh?! speak up, sonny'.upcase
-elsif speak_to_grandma =='Hi'.upcase
-puts  'huh?! speak up, sonny'.upcase
-elsif speak_to_grandma =="what did you eat today?".upcase 
-  puts 'no, not since 1938!'.upcase
-elsif speak_to_grandma =='what'.upcase 
-puts 'no, not since 1938!'.upcase
-end
-
-
-
-
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I love you too pumpkin!".upcase
+  end 
+  if phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else 
+    return "No, not since 1938!".upcase
+  end 
+end 
